01854c59af340eb22800ca3e871d4ae69dcdac04
dufmel <114317218+dufmel@users.noreply.github.com>
2023-02-28 16:10:29 -0600

    Initial Commit

---
+++ data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        title: "Space and Rocket Center",
+        location: "Huntsville, AL",
+        googleMapsUrl: "https://goo.gl/maps/oNywdEAtyGDGYujv5",
+        startDate: "Oct 11, 2022",
+        endDate: "Oct 13, 2022",
+        description: "The U.S. Space & Rocket Center in Huntsville, Alabama is a museum operated by the government of Alabama, showcasing rockets, achievements, and artifacts of the U.S. space program.",
+        imageUrl: "https://images.unsplash.com/photo-1605813640975-0ef0ad36826a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+        {
+        id: 2,
+        title: "Chimney Rock",
+        location: "Chimeny Rock, North Carolina",
+        googleMapsUrl: "https://goo.gl/maps/kLGmgxp6W5BUExds8",
+        startDate: "Oct 20, 2012",
+        endDate: "Oct 23, 2012",
+        description: "Chimney Rock at Chimney Rock State Park is a nature lover’s paradise with a collaboration of spectacular views and incredible hiking trails. They combine to create a mountain adventure experience that is postcard perfect.",
+        imageUrl: "https://www.ytravelblog.com/wp-content/uploads/2020/05/chimney-rock-nc-2.jpg"
+    },
+        {
+        id: 3,
+        title: "Biltmore Estate",
+        location: "Asheville, North Carolina",
+        googleMapsUrl: "https://goo.gl/maps/pDMNjVNdffT1okQB6",
+        startDate: "Oct 10, 2011",
+        endDate: "Oct 12, 2011",
+        description: "Biltmore Estate in Asheville, N.C. George Vanderbilt's 250-room castle is America's Largest Home and a top attraction in Asheville. Plan a visit to Biltmore House, explore 8,000 acres of grounds, or book a behind-the-scenes tour. Elegance and grandeur abound at Biltmore, America's largest home.",
+        imageUrl: "https://images.unsplash.com/photo-1666387681397-0c98be7f9a1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    }
+]
